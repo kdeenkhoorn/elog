2e9d8258249843e7af5d36e53e874edf379feb64
Klaas Eenkhoorn <klaas@phantasyworld.intern>
2018-03-29 21:56:21 +0200

    run.sh changed, apache does proxyreverse so elog bound to 127.0.0.1.

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run --name elog --restart always -d -p 8080:8080 -v /data/docker/elog:/var/lib/elog kdedesign/elog 
+docker run --name elog --restart always -d -p 127.0.0.1:8080:8080 -v /data/docker/elog:/var/lib/elog kdedesign/elog 
